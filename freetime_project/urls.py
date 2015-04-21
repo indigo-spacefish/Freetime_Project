540ce028fad76ae8b72387626580b8a01aec14fa
indigo-spacefish <brandong85@gmail.com>
2015-04-21 10:42:39 -0700

    basic models

--- freetime_project/urls.py
+++ freetime_project/urls.py
@@ -1,5 +1,8 @@
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     # Examples:
@@ -9,3 +12,15 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^freetime/', include('freetime.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        (r'^media/(?P<path>.*)',
+        'serve',
+        {'document_root': settings.MEDIA_ROOT}), )
+
+
+if not settings.DEBUG:
+        urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
