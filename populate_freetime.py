2c0d1f165845ca0f8d5d6c0e5631c7fb6a656350
indigo-spacefish <brandong85@gmail.com>
2015-05-12 11:17:05 -0700

    population script in progress

--- populate_freetime.py
+++ populate_freetime.py
@@ -1,14 +1,53 @@
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'freetime_project.settings')
-
 import django
 django.setup()
-
 from freetime.models import Profile, Activity, Category, Goal, Record
+import datetime
 
 
 def populate():
-    pass
+    add_profile(user_name="Spacefish",
+                created_date=datetime.date(2015, 5, 10),
+                last_active=datetime.datetime(2015, 5, 12, 11, 3),
+                )
+
+    add_profile(user_name="Scotro",
+                created_date=datetime.datetime(2015, 5, 12),
+                last_active=datetime.datetime(2015, 5, 12, 9, 14),
+                )
+
+    add_activity(name="Writing",
+                 user_starred=True,
+                 sessions=50,
+                 last_session=datetime.datetime(2015, 3, 20, 16, 30, 0),
+                 )
+
+    add_activity(name="Exercise",
+                 user_starred=True,
+                 sessions=8,
+                 last_session=datetime.datetime(2015, 5, 10, 10, 15),
+                 )
+
+    add_activity(name="Reading",
+                 user_starred=True,
+                 sessions=25,
+                 last_session=datetime.datetime(2015, 4, 30, 1800, 0, 0),
+                 )
+
+    add_activity(name="Bicycling",
+                 user_starred=False,
+                 sessions=0,
+                 last_session=None,
+                 )
+
+    add_activity(name="Cooking",
+                 user_starred=False,
+                 sessions=0,
+                 last_session=None,
+                 )
+
+    add_category(name="Health")
 
 
 def add_profile(user_name, created_date, last_active):
