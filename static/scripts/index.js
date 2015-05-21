1ee0f15d71406a6f99f26138d6af6e3c2d8eddd0
indigo-spacefish <brandong85@gmail.com>
2015-05-21 16:55:52 -0700

    new images for carousel

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,1 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
 
+    var carousel = document.getElementById('carousel');
+    carousel.firstElementChild.classList.add('show');
+
+
+
+});
