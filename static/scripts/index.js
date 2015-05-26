2f1f39fe57a4b6915827f4df47e007342999e9ce
indigo-spacefish <brandong85@gmail.com>
2015-05-26 14:38:20 -0700

    nav arrows

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -1,5 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
 
     var carousel = document.getElementById('carousel');
+    var currentImage = document.getElementsByClassName('show');
+
+    function carouselForward () {
+        currentImage.nextSibling.classList.add('show')
+    }
 
 });
