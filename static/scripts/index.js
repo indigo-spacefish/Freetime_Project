d0fef59cf0039885dba46b03520d9ef1e6b0cd81
indigo-spacefish <brandong85@gmail.com>
2015-05-26 15:26:39 -0700

    javascript for carousel

--- static/scripts/index.js
+++ static/scripts/index.js
@@ -2,9 +2,35 @@ document.addEventListener("DOMContentLoaded", function() {
 
     var carousel = document.getElementById('carousel');
     var currentImage = document.getElementsByClassName('show');
+    var forwardBtn = document.getElementById('forwardArrow');
+    var backBtn = document.getElementById('backArrow');
 
-    function carouselForward () {
-        currentImage.nextSibling.classList.add('show')
+
+    function carouselForward() {
+        currentImage.style.display = 'none';
+        currentImage = currentImage.nextElementSibling;
+        currentImage.style.display = 'inline-block';
     }
+
+
+    function carouselBack() {
+        currentImage.style.display = 'none';
+        currentImage = currentImage.previousElementSibling;
+        currentImage.style.display = 'inline-block';
+    }
+
+
+    forwardBtn.addEventListener('click', function (event) {
+        console.log(event);
+        console.log(this);
+        carouselForward();
+    });
+
+
+    backBtn.addEventListener('click', function (event) {
+        console.log(event);
+        console.log(this);
+        carouselBack();
+    });
 
 });
